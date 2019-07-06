42a579623f3c34e98472578f2801b9b2046e63f4
zapteryx <max@zptx.icu>
2019-07-06 19:46:24 +0800

    [also very breaking] rename msg to obj as future events will pass an object (like msg), so generalization

--- modules/ping.js
+++ modules/ping.js
@@ -4,7 +4,7 @@ msToTime = require("../bot.js").msToTime;
 module.exports.commands = ["ping", "pong"];
 module.exports.help = [{cmd: "ping", desc: "Pong!"}, {cmd: "pong", desc: "Ping?"}]
 module.exports.events = [];
-module.exports.actions = function (event, cmd, body, msg) {
+module.exports.actions = function (event, cmd, body, obj) {
   if (cmd == "ping" || cmd == "pong") {
     if (msg.member) {
       msg.channel.createMessage("Pinging...")
